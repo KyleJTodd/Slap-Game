a3e5baf81176769f24a1cbd31d311d79e94a5dfd
Kyle Todd <Kyletodd119@gmail.com>
2019-05-10 17:02:40 -0600

    MVP

--- game.js
+++ game.js
@@ -1,32 +1,59 @@
 let target = {
   name: 'Banana',
   health: 100,
-  hits: 0
+  hits: 0,
+  items: []
 }
 
 let items = {
   peel: { name: 'Peel', modifier: 2, description: 'Exposed' },
-  freeze: { name: 'Freeze', modifier: 4, description: 'Tough Banana' },
-  blender: { name: 'Blender', 'modifier 20', description: 'WHHIIRR, WHHIIIRR' }
+  freeze: { name: 'Freeze', modifier: -4, description: 'Cold Banana' },
+  blender: { name: 'Blender', modifier: 20, description: 'WHHIIRRRRR' }
 }
 
+
+
+
+
+
 function slap() {
-  target.health--
+  target.health -= 1 + addMods()
   target.hits++
   update()
 }
 
 function punch() {
-  target.health -= 5
+  target.health -= 5 + addMods()
   target.hits++
   update()
 }
 
 function kick() {
-  target.health -= 10
+  target.health -= 10 + addMods()
   target.hits++
   update()
 }
+
+function givePeel() {
+  target.items.push(items.peel)
+}
+function giveFreeze() {
+  target.items.push(items.freeze)
+}
+function giveBlender() {
+  target.items.push(items.blender)
+}
+
+
+function addMods() {
+  let out = 0
+  for (let i = 0; i < target.items.length; i++) {
+    let modifier = target.items[i].modifier;
+    out += modifier
+  }
+  return out
+}
+
 
 function update() {
   document.getElementById('health').innerText = target.health
